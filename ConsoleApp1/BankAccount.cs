4a8bd895c42c2d97e6fdefee62051919e29a8428
Hemal99 <h.githmin@gmail.com>
2022-10-16 18:57:35 +0530

    Add project files.

---
+++ ConsoleApp1/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+         
+        public decimal Balance 
+        {
+            get
+            {
+
+                decimal balance = 0;
+
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 123456789;
+
+        private List<Transaction> allTransactions  = new List<Transaction>();
+
+        public BankAccount(string name)
+        {
+            this.Owner = name;
+         
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+
+        }
+
+        public void MakeWithdrawal(decimal amount,DateTime date, string note)
+        {
+             if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+
+            }
+             if(Balance -amount <= 0)
+            {
+                throw new InvalidOperationException("Insufficient Balance");
+            }
+
+             var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ConsoleApp1;
+using System;
+
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("kendra", 1000);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
